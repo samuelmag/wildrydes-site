4d1ead90320eca8c4c6e0c84b0f0a0a0224ce53d
samuelmag <81941624+samuelmag@users.noreply.github.com>
2025-06-02 11:29:07 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_y5Jl8kIzd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '186o3cmu0or3uhcmfjhodqcna5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-1_hkdqS6vPW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1of8aqcb7qaditd5n0mcek9qku', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
